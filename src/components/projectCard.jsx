dd75953e50faa176e0dffbffc9b2e2befca7e9be
DRISHTI Jadhwani <jadhwanidrishti2004@gmail.com>
2025-06-07 16:07:54 +0530

    add github links

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -1,7 +1,7 @@
 const Card=(Props)=>{
 return(
     <div className="bg-transparent w-100 h-auto rounded-2xl  p-8 hover:scale-105 border-fuchsia-500 shadow-fuchsia-500 shadow-md   transition-transform ease-in-out duration-100 delay-75 hover:shadow-fuchsia-100  hover:shadow-[inset_6px_6px_12px_#bebebe,inset_-6px_-6px_12px_#ffffff]" >
-      <div className="flex items-center justify-center relative">
+      <div className="flex items-center justify-center">
     <img src={Props.img} className="w-full h-50 rounded-2xl" alt={Props.title}></img>
       </div>
     
@@ -13,7 +13,10 @@ return(
 
       </div>  
             <div className="flex justify-center">
-        <button className="right-2 bg-fuchsia-500 text-white md:p-2 p-2 mt-4  rounded-lg md:text-xl text-lg">Get Code</button>
+              <a href={Props.link}>
+        <button className="right-2 bg-fuchsia-500 text-white md:p-2 p-2 mt-4  rounded-lg md:text-xl text-lg cursor-pointer">Get Code</button>
+                </a>
+
       </div>
 
 
